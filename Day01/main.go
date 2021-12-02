de0d61a30ee1dbb4a8f9216cb40108376b3483aa
Ben <bengunton@btinternet.com>
2021-12-02 00:25:54 +0000

    Day 1 part 2

--- Day01/main.go
+++ Day01/main.go
@@ -16,7 +16,7 @@ func main() {
 	}
 	defer file.Close()
 
-	fmt.Printf("Counted %d increases in %s", CountDepthIncreases(file), fileName)
+	fmt.Printf("Counted %d increases in %s", CountThreeWindowDepthIncreases(file), fileName)
 }
 
 func CountDepthIncreases(reader io.Reader) int {
@@ -37,6 +37,42 @@ func CountDepthIncreases(reader io.Reader) int {
 		if currentVal > prevVal {
 			numIncreases++
 		}
+	}
+
+	return numIncreases
+}
+
+func CountThreeWindowDepthIncreases(reader io.Reader) int {
+	scanner := bufio.NewScanner(reader)
+
+	numIncreases := 0
+	var prevSum int
+	var currSum int
+
+	// first line
+	scanner.Scan()
+	reading1 := readLine(scanner)
+
+	// second line
+	scanner.Scan()
+	reading2 := readLine(scanner)
+
+	// third line
+	scanner.Scan()
+	reading3 := readLine(scanner)
+
+	currSum = reading1 + reading2 + reading3
+	// remaining lines
+	for scanner.Scan() {
+		prevSum = currSum
+
+		reading1, reading2, reading3 = reading2, reading3, readLine(scanner)
+		currSum = reading1 + reading2 + reading3
+
+		if currSum > prevSum {
+			numIncreases++
+		}
+
 	}
 
 	return numIncreases
--- Day01/main_test.go
+++ Day01/main_test.go
@@ -17,3 +17,16 @@ func TestCountDepthIncreases(t *testing.T) {
 		t.Errorf("Expected 7 increases, got %d", numIncreases)
 	}
 }
+
+func TestCountThreeWindowDepthIncreases(t *testing.T) {
+	test, err := os.Open("./sample")
+	if err != nil {
+		panic(err)
+	}
+	defer test.Close()
+
+	numIncreases := CountThreeWindowDepthIncreases(test)
+	if numIncreases != 5 {
+		t.Errorf("Expected 5 increases, got %d", numIncreases)
+	}
+}
