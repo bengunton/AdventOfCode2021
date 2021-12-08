4d5d4762fc02ced098ec6b6482d5eb0fcbf2eceb
Ben <bengunton@btinternet.com>
2021-12-08 22:10:39 +0000

    Day 07 part 2

--- Day07/main.go
+++ Day07/main.go
@@ -11,7 +11,7 @@ func main() {
 	rows := util.ReadFileArg()
 	positions := util.ParseInts(rows[0])
 
-	findCheapestPoint(positions)
+	findCheapestPointV2(positions)
 }
 
 func findCheapestPoint(positions []int) {
@@ -20,6 +20,17 @@ func findCheapestPoint(positions []int) {
 
 	fuelCost := calculateFuelCost(midpoint, positions)
 	fmt.Println("Cost:", fuelCost)
+}
+
+func findCheapestPointV2(positions []int) {
+	midpoint, otherMidpoint := findMeans(positions)
+	fmt.Println("Midpoint:", midpoint)
+	fmt.Println("Midpoint2:", otherMidpoint)
+
+	fuelCost := calculateFuelCostV2(midpoint, positions)
+	fuelCost2 := calculateFuelCostV2(otherMidpoint, positions)
+	fmt.Println("Cost:", fuelCost)
+	fmt.Println("Cost2:", fuelCost2)
 }
 
 func calculateFuelCost(midpoint int, positions []int) int {
@@ -32,11 +43,31 @@ func calculateFuelCost(midpoint int, positions []int) int {
 	return cost
 }
 
+func calculateFuelCostV2(midpoint int, positions []int) int {
+	cost := 0
+
+	for _, pos := range positions {
+		cost += costForDistanceV2(abs(pos - midpoint))
+	}
+
+	return cost
+}
+
 func findMedian(positions []int) int {
 	sort.Ints(positions)
 
 	midpoint := positions[len(positions)/2]
 	return midpoint
+}
+
+func findMeans(positions []int) (int, int) {
+	sum := 0
+	for _, pos := range positions {
+		sum += pos
+	}
+
+	mid := sum / len(positions)
+	return mid, mid + 1
 }
 
 func abs(num int) int {
@@ -45,4 +76,8 @@ func abs(num int) int {
 	} else {
 		return num
 	}
+}
+
+func costForDistanceV2(num int) int {
+	return (num * (num + 1)) / 2
 }
